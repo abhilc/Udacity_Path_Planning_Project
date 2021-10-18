4ee0b6d8ce82f14b49839de76b70e344c8539bff
Abhishek <abhishek.kusnoor@gmail.com>
2021-10-18 21:15:48 +0000

    Planner points computation

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -92,11 +93,123 @@ int main() {
 
           vector<double> next_x_vals;
           vector<double> next_y_vals;
-
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+          
+          //Give ID to ego lane for convenience. Left Lane = 0, Ego Lane = 1 and Right Lane = 2. Start with Ego Lane
+          int lane = 1;
+          
+          double ref_vel = 49.5; //Reference Velocity
+          
+          int prev_size = previous_path_x.size();
+          
+          vector<double> ptsx, ptsy;
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+          
+          if(prev_size < 2)
+          {
+            //Use points that are tangent to the car
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+            
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+            
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }
+          else
+          {
+            //Just append to the existing path 
+            ref_x = previous_path_x[prev_size-1];
+            ref_y = previous_path_y[prev_size-1];
+            
+            double ref_x_prev = previous_path_x[prev_size-2];
+            double ref_y_prev = previous_path_y[prev_size-2];
+            
+            ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+            
+            //Use two points that make the path from the previous point a tangent 
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+            
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+          
+          //Get XY pairs 30m apart in frenet co-ordinates
+          vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+          
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+          
+          //Transform each point to vehicle fixed co-ordinate system(Car's perspective)
+          
+          for(int i=0;i<ptsx.size();i++)
+          {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+            
+            ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y * sin(0-ref_yaw));
+            ptsy[i] = (shift_y * cos(0-ref_yaw) + shift_y * sin(0-ref_yaw));
+          }
+          
+          //Debug
+          for(int i=0;i<ptsx.size();i++)
+          {
+            printf("%.3f\n",ptsx[i]);
+          }
+          
+          //Create a spline
+          tk::spline s;
+          s.set_points(ptsx, ptsy);
+          
+          //Populate actual path planner vectors
+          
+          for(int i=0;i<previous_path_x.size();i++)
+          {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+          
+          //Compute a 30m spline section
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt((target_x*target_x) + (target_y*target_y));
+          
+          double x_add_on = 0.0;
+          
+          //Fill the remainder of the previous points to the path planner
+          for(int i=1;i<=50-previous_path_x.size();i++)
+          {
+            double N = (target_dist/(0.02*ref_vel/2.24));
+            double x_point = x_add_on + (target_x/N);
+            double y_point = s(x_point);
+            
+            x_add_on = x_point;
+            
+            double x_ref = x_point;
+            double y_ref = y_point;
+            
+            //rotate back to normal after rotating it earlier
+            x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+            y_point = x_ref * cos(ref_yaw) + y_ref * sin(ref_yaw);
+            
+            x_point += ref_x;
+            y_point += ref_y;
+            
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
+          
+          
 
 
           msgJson["next_x"] = next_x_vals;
